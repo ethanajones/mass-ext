92333c461a63525ba50b3af48ad41fc16d0cce46
EAJ <eaj9990@gmail.com>
2023-04-20 12:11:00 -0700

    change to red and convert to yarn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#020617] to-[#64748b]">
         <div>
-          <h1 className="text-[5rem] hover:text-[#94a3b8] text-[#f1f5f9]">Nope</h1>
+          <h1 className="text-[5rem] hover:text-[#fca5a5] text-[#f1f5f9]">Nope</h1>
         </div>
       </main>
     </>
